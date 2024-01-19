87de5b4eda75876136eeceb0f100d900319dcaee
ALIEN MASTER <6527923+alienmaster9@users.noreply.github.com>
2024-01-20 03:56:21 +0700

    Update start-squid.sh

--- start-squid.sh
+++ start-squid.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Generate Squid configuration file with dynamic port and hostname
-echo "http_port 0.0.0.0:9999" > /etc/squid/squid.conf
+echo "http_port 0.0.0.0:$PORT" > /etc/squid/squid.conf
 
 # Start Squid
 squid -N
