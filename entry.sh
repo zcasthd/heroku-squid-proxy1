acaa6bf28f9ccf86df5b82178a3dce204a52561d
ALIEN MASTER <6527923+alienmaster9@users.noreply.github.com>
2024-01-19 23:32:13 +0700

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -3,14 +3,7 @@ set -e
 
 
 
-# allow arguments to be passed to squid
-if [[ ${1:0:1} = '-' ]]; then
-  EXTRA_ARGS="$@"
-  set --
-elif [[ ${1} == squid || ${1} == $(which squid) ]]; then
-  EXTRA_ARGS="${@:2}"
-  set --
-fi
+
 
 # default behaviour is to launch squid
 if [[ -z ${1} ]]; then
